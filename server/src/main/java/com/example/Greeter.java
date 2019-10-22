8f75201896e0dff6846be243948cad7ebce66a9e
rauldejuan <56826151+rauldejuan@users.noreply.github.com>
2019-10-22 12:42:30 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,8 +16,8 @@ public class Greeter {
    * Metodo de greet modificado.
    */
   public String greet(String someone) {
-    //Long lalala = new Long("2333");
-    if(MAGICO>1){
+    Long lalala = new Long("2333");
+    if(true){
       return String.format("Hello, %s!", someone);}
     else{
         return String.format("Hello, %s!", someone);
